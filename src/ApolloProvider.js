c18ac65238f280e5ac54e40700b6a2df6b5b145b
adeola adeyemo <adeoladeyemo@gmail.com>
2019-12-17 13:17:51 +0100

    add proxy and change http link to new server url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { setContext } from 'apollo-link-context';
 import App from './App';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000/'
+  uri: 'https://postapp-backend.herokuapp.com/'
 });
 
 const authLink = setContext((req, prevContext) => {
